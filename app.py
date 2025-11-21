1e36648161d5052946f2def08d5ab9904add1553
Brody Van Wave <brodyvanwave@gmail.com>
2025-11-20 22:37:57 -0500

    Co-authored-by: Owen Wang <NewoIsTaken@users.noreply.github.com> Co-authored-by: briantollar <briantollar@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello, Brian!</p>"
+    return render_template("index.html")
+
+
+@app.route("/location")
+def location():
+    return render_template("location.html")
+
+
+@app.route("/entree-select")
+def entree_select():
+    return render_template("entree-select.html")
+
+
+@app.route("/rate")
+def rate():
+    return render_template("rate.html")
+
+
+@app.route("/menu")
+def menu():
+    return render_template("index.html")
+    
+
+@app.route("/reviews")
+def reviews():
+    return render_template("reviews.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
